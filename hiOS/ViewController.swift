9058be72fde7ff4d224a752a33d4756584238a4e
Justin Inouye <inouju@gmail.com>
2018-03-03 03:59:17 -0800

    Add a helper to download and parse JSON

--- hiOS/ViewController.swift
+++ hiOS/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // FIXME: Remove example loading when unneeded
+        let c = CoinAPIHelper()
+        c.update()
     }
 
     override func didReceiveMemoryWarning() {
