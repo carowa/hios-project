fb8e0b64790d9baae75c2bb5bda6556a4d707e73
Justin Inouye <inouju@gmail.com>
2018-03-04 16:32:44 -0800

    Convert comments to Markdown doc style in Favorites.swift

--- hiOS/Favorites.swift
+++ hiOS/Favorites.swift
@@ -14,19 +14,32 @@ class FavoritesRepo {
     
     private var list : [String] = []
     
-    // Adds a new element to FavoritesRepo
+    /**
+     Adds a new element to FavoritesRepo
+     
+     - Parameter name: Name of favorite cryptocurrency to add
+    */
     func add(name : String) {
         list.append(name)
     }
     
-    // Removes an element from FavoritesRepo
+    /**
+     Removes an element from FavoritesRepo
+     
+     - Parameter name: Name of favorite cryptocurrency to remove
+    */
     func remove(name : String) {
         let index = findItem(name: name)
+        // FIXME: This will crash if findItem returns -1.
         list.remove(at: index)
     }
     
-    // Helper function to find the index of element to remove
-    // Returns -1 if the element is not found
+    /**
+     Helper function to find the index of an element
+     
+     - Parameter name: Name of the favorite cryptocurrency to find
+     - Returns: The index of the element or -1 if the element is not found
+    */
     private func findItem(name : String) -> Int {
         for i in 0...list.count {
             let e : String = list[i]
